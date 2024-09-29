7b885bc7e07ba686fbb2c5e36a080d74a58a63f4
Aaron Anderson <aace2@me.com>
2024-09-29 12:45:45 -0700

    Ch. 5.3 Sheets, Lists, and Building the To Do List User Interface

--- ToDoList/Views/DetailView.swift
+++ ToDoList/Views/DetailView.swift
@@ -8,32 +8,63 @@
 import SwiftUI
 
 struct DetailView: View {
-    var passedValue: String // Don't initialize - it will be passed from the parent view
+    @State var toDo: String
+    @State private var reminderIsOn = false
+//    @State private var dueDate = Date.now + 60*60*24
+    @State private var dueDate = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
+    @State private var notes = ""
+    @State private var isCompleted = false
+    
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack {
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
+        List {
+            TextField("Enter The Dragon", text: $toDo)
+                .font(.title)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+                .listRowSeparator(.hidden)
             
-            Text("velocity = ⁠dr/dt!\nAnd you passed over the value \(passedValue) ")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
+            Toggle("Set Reminder:", isOn: $reminderIsOn)
+                .padding(.top)
+                .listRowSeparator(.hidden)
             
-            Spacer()
+            DatePicker("Date:", selection: $dueDate)
+                .listRowSeparator(.hidden)
+                .padding(.bottom)
+                .disabled(!reminderIsOn)
             
-            Button("Get Back!") {
-                dismiss()
-            }
-            .buttonStyle(.borderedProminent)
+            Text("Notes:")
+                .padding(.top)
+            
+            TextField("Notes", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .listRowSeparator(.hidden)
+            
+            Toggle("Completed:", isOn: $isCompleted)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            
         }
-        .padding()
-        //.navigationBarBackButtonHidden()
+        .listStyle(.plain)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    //TODO: Add Save Code Here
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    DetailView(passedValue: "Item 1")
+    NavigationStack {
+        DetailView(toDo: "")
+    }
 }
--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -15,20 +15,37 @@ struct ToDoListView: View {
                  "Vermax"
     ]
     
+    @State private var sheetIsPresented = false
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(toDos, id: \.self) { toDo in
                     NavigationLink {
-                        DetailView(passedValue: toDo)
+                        DetailView(toDo: toDo)
                     } label: {
                         Text(toDo)
                     }
+                    .font(.title2)
                 }
             }
-            .navigationBarTitle("To Do List")
+            .navigationBarTitle("Dragon List")
             .navigationBarTitleDisplayMode(.automatic)
             .listStyle(.plain)
+            .sheet(isPresented: $sheetIsPresented) {
+                NavigationStack {
+                    DetailView(toDo: "")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
     }
 }
